bbfecaf62c4204dfbffbc867ac1762c5847e122d
Yang Zhengquan <yangzhq0@live.com>
2025-05-10 22:11:52 +0800

    feat: add DataCubeAPI import and update version to 0.1.2

--- tushare_plus/__init__.py
+++ tushare_plus/__init__.py
@@ -1,7 +1,7 @@
 # Tushare Plus
 # 增强版Tushare API客户端
 
-from .client import TushareAPI, APILimitDetector
+from .client import TushareAPI, APILimitDetector, DataCubeAPI
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __all__ = ['TushareAPI', 'APILimitDetector']

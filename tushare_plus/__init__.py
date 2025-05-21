ede5778db24005329bccb32e95085fa493da50b3
Yang Zhengquan <yangzhq0@live.com>
2025-05-22 00:36:41 +0800

    chore: update version and add DataCubeAPI to __all__
    Update the package version to '0.1.4' and include 'DataCubeAPI' in the __all__ list to ensure it is exported properly

--- tushare_plus/__init__.py
+++ tushare_plus/__init__.py
@@ -3,5 +3,5 @@
 
 from .client import TushareAPI, APILimitDetector, DataCubeAPI
 
-__version__ = '0.1.3'
-__all__ = ['TushareAPI', 'APILimitDetector']
+__version__ = '0.1.4'
+__all__ = ['TushareAPI', 'APILimitDetector', 'DataCubeAPI']
